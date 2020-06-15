e857deb4a297d9e811d41d1fba603bdedc9e6ed4
fatfat123-web <653694078@qq.com>
2020-06-16 00:50:57 +0800

    点击事件和长按事件以及触摸结束事件

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,158 @@
 <template>
-    $END$
+    <div class="scroll" ref="scroll" @scroll="scroll">
+        <div class="main" @touchstart.prevent="gtouchstart"  @touchend.prevent="triggerReply" @touchmove="touchmove">
+
+            <img src="../assets/images/bj.jpg" style="width: 100%;height: auto;"/>
+
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "index"
+        name: "index",
+        data() {
+
+            return {
+                //前进
+                front: null,
+                //左边区域
+                rear: null,
+                x: null,
+                Y: null,
+                startX: null,
+                startY: null,
+                Loop: 0,
+                test: 0,
+                timer: null,
+                flagfind:false,
+            }
+        },
+        mounted() {
+        },
+        methods: {
+            scroll(e) {
+                //滚动条拖动的长度
+                const scrollTop = e.target.scrollTop
+                //网页正文全文高，包括有滚动条时的未见区域
+                let scrollHeight = this.$refs.scroll.scrollHeight
+                // 可视窗口的高度
+                let clientHeight = this.$refs.scroll.clientHeight
+                //减去滚动条可拖动的网页长度
+                let Height = scrollHeight - clientHeight
+                //滚动条滚动距离 百分比
+                let scrolldrag = scrollTop / Height
+                let _scrolldrag = (scrolldrag * 100).toFixed(2) * 1
+                this.gkd = _scrolldrag;
+                // console.log(clientHeight)
+
+            },
+
+            // 长按事件
+            gtouchstart(e) {
+                let clientHeight = this.$refs.scroll.clientHeight
+                this.startX = e.changedTouches[0].pageX;
+                this.startY = e.changedTouches[0].pageY;
+                this.front = (clientHeight) * (0.4);
+                let div = this.$refs.scroll
+                //执行长按的内容
+
+                //执行长按的内容
+                this.Loop = setTimeout( ()=> {
+                      this.loop=0;
+                    // console.log(this.front)
+                    if (this.startY < this.front) {
+                        this.retreat()
+                        console.log( div.scrollTop)
+                    }else {
+                        this.advance()
+                        console.log( div.scrollTop)
+                    }
+
+                }, 500);
+                return false;
+            },
+            //   前进
+            advance() {
+
+                console.log(this.flagfind,'前进了')
+                let div = this.$refs.scroll
+                div.scrollTop += 2
+                window.requestAnimationFrame(this.advance)
+                    if (this.flagfind===true)
+                    return false;
+                    // this.timer = setInterval(() => {
+                    //     console.log(this.timer)
+                    //     this.test++;
+                    //     console.log(this.test)
+                    // }, 1000)
+
+
+            },
+            // 后退
+            retreat(){
+                let div = this.$refs.scroll
+                div.scrollTop -= 2
+                console.log('后退了')
+                window.requestAnimationFrame(this.retreat)
+                if (this.flagfind===true)
+                    return false;
+            },
+
+
+            //
+            triggerReply() {
+                // let div = this.$refs.scroll
+                const self = this;
+                // clearTimeout(this.timer);
+                // this.flagfind=true;
+                // //这里click内容
+                // div.scrollTop += 2
+                if (self.Loop !== 0) {
+                    console.log('点击事件');
+                    this.flagfind=true;
+                }
+                // return false;
+            },
+
+
+
+            touchmove(e) {
+                e.preventDefault();
+                if (e.changedTouches.length) {
+                    //移动时的坐标
+                    let moveEndX = e.changedTouches[0].pageX;
+                    let moveEndY = e.changedTouches[0].pageY;
+                    //计算当前的坐标跟初始坐标的差值
+                    this.x = moveEndX - this.startX;
+                    this.y = moveEndY - this.startY;
+                }
+            },
+
+        },
+
     }
 </script>
 
-<style scoped>
 
+<style scoped lang="scss">
+    img {
+        pointer-events: none; /* 禁止长按图片保存 */
+
+    }
+
+    .scroll {
+        width: 100%;
+        height: 100vh;
+        margin: 0 auto;
+        overflow-y: auto;
+        overflow-x: hidden;
+        background: url(../assets/images/bj.jpg) no-repeat bottom;
+        background-size: 100%;
+    }
+
+    .main {
+        position: relative;
+
+    }
 </style>
