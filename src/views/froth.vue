99ced5e3ca4a6894cb1ba5a9f600678048162748
weijie <615777579@qq.com>
2020-06-19 17:31:24 +0800

    update

--- src/views/froth.vue
+++ src/views/froth.vue
@@ -1,9 +1,9 @@
 <template>
 
-        <section>
+    <section>
 
-            <h2>Bubbles</h2>
-        </section>
+        <h2>Bubbles</h2>
+    </section>
 
 </template>
 
@@ -20,29 +20,30 @@
                 const section = document.querySelector('section')
                 const createElement = document.createElement('span')
                 var size = Math.random() * 60;
-                createElement.style.width =50+ size + 'px';
-                createElement.style.height =50+ size + 'px';
-                createElement.style.left= Math.random() * innerWidth + "px";
+                createElement.style.width = 50 + size + 'px';
+                createElement.style.height = 50 + size + 'px';
+                createElement.style.top= Math.random() * innerHeight + "px";
                 section.appendChild(createElement);
-                setTimeout(()=>{
+                setTimeout(() => {
 
                     createElement.remove()
 
-                },4000)
+                }, 4000)
             }
-            setInterval(createBubble,50)
+
+            setInterval(createBubble, 50)
         },
         methods: {
 
-aa(){
+            aa() {
 
 
-}
+            }
         }
     }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 
 
     section {
@@ -56,27 +57,30 @@ aa(){
         align-items: center;
 
     }
-    section h2{
+
+    section h2 {
         font-size: 1rem;
         color: #333;
     }
-    section span{
+
+    section span {
         position: absolute;
-        bottom: -50px;
+        left: -50px;
         background: transparent;
         border-radius: 50%;
         pointer-events: none;
-        box-shadow:inset 0 0 10px rgba(225,225,225,0.5);
-        animation:animate 4s linear infinite;
+        box-shadow: inset 0 0 10px rgba(225, 225, 225, 0.5);
+        animation: animate 4s linear infinite;
     }
+
     @keyframes animate {
-        0%{
-            transform: translateY(0%);
+        0% {
+            transform: translateX(0%);
             opacity: 1;
         }
-        100%{
+        100% {
 
-            transform: translateY(-1200%);
+            transform: translateX(1200%);
             opacity: 1;
         }
 
